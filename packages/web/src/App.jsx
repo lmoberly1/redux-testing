b82ffed56b5bd5c6b4343526831e1103cad954ef
Luke Moberly <lukemoberly@gmail.com>
2020-12-21 16:12:09 -0600

    Add register component

--- packages/web/src/App.jsx
+++ packages/web/src/App.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Components
+import RegisterForm from './auth/RegisterForm';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Hi</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/register" component={RegisterForm} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ packages/web/src/auth/RegisterForm.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+export default function RegisterForm() {
+  return <Button>Press Me</Button>;
+}
