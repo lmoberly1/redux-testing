de1d058962635829dafe5b389fd31c9058610c73
lmoberly1 <lukemoberly@gmail.com>
2020-12-21 17:13:57 -0600

    Add header for git test

--- packages/web/src/auth/RegisterForm.jsx
+++ packages/web/src/auth/RegisterForm.jsx
@@ -2,5 +2,11 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 
 export default function RegisterForm() {
-  return <Button>Press Me!!</Button>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Button>Press Me!!</Button>
+    </div>
+  )
+  ;
 }
