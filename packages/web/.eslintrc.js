3af1d87310b8f65ccc89e551d0d8708e15702270
lmoberly1 <lukemoberly@gmail.com>
2020-12-23 09:29:57 -0600

    Eslint test

--- packages/web/.eslintrc.js
+++ packages/web/.eslintrc.js
@@ -1,27 +1,28 @@
-module.exports = {
-    "env": {
-        "browser": true,
-        // "es2021": true
+export default {
+  "env": {
+    "browser": true,
+    // "es2021": true
+  },
+  "extends": [
+    "airbnb",
+    "airbnb/hooks",
+    "airbnb-base",
+    "eslint:recommended",
+    "prettier",
+    // "plugin:react/recommended"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
     },
-    "extends": [
-        "airbnb",
-        "airbnb/hooks",
-        // "airbnb-base",
-        // "eslint:recommended",
-        // "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "indent": "off",
-        "arrow-body-style": ["error", "as-needed"]
-    }
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react",
+    "prettier"
+  ],
+  "rules": {
+    "no-unused-vars": "warn"
+  }
 };
--- packages/web/src/actions/index.jsx
+++ packages/web/src/actions/index.jsx
@@ -1,3 +1,5 @@
 export const increment = (number) => ({ type: 'INCREMENT', payload: number });
 
 export const decrement = (number) => ({ type: 'DECREMENT', payload: number });
+
+console.log('hi');
